8c40a3c7675fa071baf20893791b555831afec72
ronilojdelrosario <ronilojdelrosario@gmail.com>
2026-04-03 17:09:16 +0800

    slightly modified layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://ronilojdelrosario.github.io/'
+  site: 'https://ronilojdelrosario.github.io/',
+  experimental: {
+    fonts: [{
+      provider: fontProviders.fontsource(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
+  }]}
 });
